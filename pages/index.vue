51f80a6fb176542de72cf20e3be09a637b21b9f6
KubaM-97 <jakubm16@interia.pl>
2021-11-19 12:40:41 +0100

    z

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,53 @@
 <template>
 <div>INDEX
-    <GroupView />
+    GroupView: {{students}} KONIEC
+    <!-- <GroupView /> -->
 </div>
 </template>
 
 <script>
-import { defineComponent, useRouter, useStore } from '@nuxtjs/composition-api'
-import GroupView from "@/views/GroupView.vue";
+import { defineComponent, useRouter, useStore,useContext, useFetch } from '@nuxtjs/composition-api'
+// import GroupView from "@/views/GroupView.vue";
 
 export default defineComponent({
     components:{
-        GroupView,
+        // GroupView,
     },
-    setup() {
+    setup(_p, {root}) {
         const store = useStore()
         const router = useRouter()
         // if(store.getters.isAuthenticated) router.push({ path: `group/${store.getters.loggedInUser.group}` })
+         const students = ref([]);
+    const { $http } = useContext();
+    useFetch(async () => {
+      try{
+        students.value = await $http.$post(`/api/students/`, 
+        { group: root.$auth.user.group },{
+        headers:{
+          Authorization: root.$auth.strategy.token.get()
+        }}
+      )
+       root.$toast.success('pobrano')
+      } catch (error) {
+        const status = error.response.status;
+        switch (status) {
+          case 404: {
+            root.$toast.error(root.$t("failed_to_fetch_students_list"));
+            break;
+          }
+          case 500: {
+            root.$toast.error(root.$t("server_error"));
+            break;
+          }
+          default: {
+            root.$toast.error(root.$t("alternative_log_error"));
+          }
+        }
+      }
+    })
+    return {
+        students
+    }
     },
 })
 </script>
