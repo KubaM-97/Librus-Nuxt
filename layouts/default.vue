dc5988d2523952124cb3669f308b398d6bec74b0
KubaM-97 <jakubm16@interia.pl>
2021-11-19 12:23:40 +0100

    z

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- <TheHeader v-if="$store.getters.isAuthenticated"/> -->
-    <!-- <TheHeader /> -->
+    <TheHeader v-if="$auth.loggedIn"/>
     LoggedIn: {{$auth.loggedIn}}
     <!-- Loggeduser: {{$auth.user}} -->
     <Nuxt />
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-<div>INDEX</div>
+<div>INDEX
+    <GroupView />
+</div>
 </template>
 
 <script>
 import { defineComponent, useRouter, useStore } from '@nuxtjs/composition-api'
+import GroupView from "@/views/GroupView.vue";
 
 export default defineComponent({
+    components:{
+        GroupView,
+    },
     setup() {
         const store = useStore()
         const router = useRouter()
