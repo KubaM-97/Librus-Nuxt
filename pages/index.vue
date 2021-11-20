a7aad86cef4098279c45c51bbc5c13c5bd32c577
KubaM-97 <jakubm16@interia.pl>
2021-11-20 15:03:50 +0100

    fix redirecting error

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,11 @@
 </template>
 
 <script>
-import { defineComponent, useStore, useRouter } from '@nuxtjs/composition-api';
+import { defineComponent, useRouter } from '@nuxtjs/composition-api';
 export default defineComponent({
     setup(_p, {root}) {
-        const store = useStore()
         const router = useRouter()
-        if(store.getters.isAuthenticated) router.push({ path: `group/${root.$auth.user.group}` })
+        if(root.$auth.loggedIn) router.push({ path: `group/${root.$auth.user.group}` })
     },
 })
 </script>
