8b59dbbba45862dd973dcd0948437cf0017caec0
KubaM-97 <jakubm16@interia.pl>
2021-11-20 10:39:18 +0100

    dynamic route

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
+<GroupView />
 </template>
 
 <script>
 import { defineComponent, useStore, useRouter } from '@nuxtjs/composition-api';
+import GroupView from '@/views/GroupView'
 export default defineComponent({
+    components: {GroupView},
     setup(_p, {root}) {
         const store = useStore()
         const router = useRouter()
-        if(store.getters.isAuthenticated) router.push({ path: `group/${store.getters.loggedInUser.group}` })
+        // if(store.getters.isAuthenticated) router.push({ path: `group/${store.getters.loggedInUser.group}` })
     },
 })
 </script>
